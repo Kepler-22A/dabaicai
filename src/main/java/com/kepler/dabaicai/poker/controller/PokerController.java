e931894b5ee6842df15db2504c1aad60c2375d5e
LiuShiYang <1642558447@qq.com>
2024-05-07 19:25:50 +0800

    测试项目

--- src/main/java/com/kepler/dabaicai/poker/controller/PokerController.java
+++ src/main/java/com/kepler/dabaicai/poker/controller/PokerController.java
@@ -1,6 +1,7 @@
 package com.kepler.dabaicai.poker.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -30,5 +31,15 @@ public class PokerController {
             System.out.println(jo.get(key));
         }
         return "{\"retCode\":1,\"errMsg\":\"转换成功！\"}";
+    }
+
+    @RequestMapping(value = "tell-me-sex")
+    @ResponseBody
+    public String tellMeSex(HttpServletRequest request){
+        String sex = request.getParameter("sex");
+        if (StringUtils.isBlank(sex)){
+            return "你没有告诉我性别！";
+        }
+        return "好的，你的性别是："+sex;
     }
 }
